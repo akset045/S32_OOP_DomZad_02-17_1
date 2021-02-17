22a2f2dda1202e51703c7bc3fc92eb0c1cdf51e0
Akset045 <Akset045@gmail.com>
2021-02-17 20:52:46 +0300

    Add project files.

---
+++ DomZad_02-17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomZad_02_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите число A: ");
+            Double A = Double.Parse(Console.ReadLine());
+
+            Console.Write("Введите число B: ");
+            Double B = Double.Parse(Console.ReadLine());
+
+            Console.Write("Сумма чисел: " + (A + B));
+            Console.WriteLine();
+            Console.Write("Разность чисел: " + (A - B));
+
+            Console.ReadKey();
+        }
+    }
+}
